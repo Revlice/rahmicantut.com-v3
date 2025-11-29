f8a443114e031e4b4f162f1d89644c6b51c81465
Rahmican Tut <124275194+Revlice@users.noreply.github.com>
2025-11-29 20:56:42 +0300

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,23 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
+function DateRedirect() {
+  window.location.href = "/date.html"; // public/date.html
+  return null;
+}
+
 const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<App/>,
-        errorElement:<ErrorPage/>
-    }
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/date",
+    element: <DateRedirect />,
+    errorElement: <ErrorPage />
+  }
 ]);
+
 export default router;
